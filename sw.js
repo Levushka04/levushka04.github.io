4467d3e92cda5b350eada09f32a66663afd40c41
Levushka04 <140205752+Levushka04@users.noreply.github.com>
2023-07-21 22:50:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["proba verstka.html","style.css"]); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
